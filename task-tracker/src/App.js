416f3d37951dd1d11c10cba8d78b1edb4f933d8b
pnichols789 <pnichols789@gmail.com>
2022-10-18 19:10:09 -0500

    -

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     
     <div>
-      <Container maxWidth='md'>
-        <TaskItem/>
+      <Container maxWidth="md">
+        <TaskItem />
 
       </Container>
     </div>
--- task-tracker/src/components/TaskItem/TaskItem.jsx
+++ task-tracker/src/components/TaskItem/TaskItem.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Checkbox, ListItem, ListItemText } from "@mui/material";
+import { Checkbox, ListItem, ListItemText } from '@mui/material';
 import { MdCheckCircle, MdOutlineCircle } from "react-icons/md";
 import './TaskItem.scss';
 
 const TaskItem = () => {
-	return (
-		<>
-			<ListItem disablePadding className="TaskItem">
-				<Checkbox
-					icon={<MdOutlineCircle className="checkIcon" />}
-					checkedIcon={<MdCheckCircle className="checkIcon" />}
-				/>
-				<ListItemText primary="Walk the dog" secondary="March 29, 2022"/>
-			</ListItem>
-		</>
-	);
-};
+  return (
 
-export default TaskItem;
+    <>
+      <ListItem disablePadding className="TaskItem">
+        <Checkbox
+          icon={ <MdOutlineCircle className='checkIcon' /> }
+          checkedIcon={ <MdCheckCircle className="checkIcon" /> }
+        />
+        <ListItemText primary="Walk the dog" secondary="March 29 2022"/>
+      </ListItem>
+    </>
+  )
+}
+
+export default TaskItem
